87edf67d8e59365e1aed4791b128034a944c54d9
choco-minion <114739562+choco-minion@users.noreply.github.com>
2023-05-20 11:47:40 +0900

    Update logic.py

--- code/Q2sort/logic.py
+++ code/Q2sort/logic.py
@@ -1,16 +1,24 @@
 class Logic():
 
-  def sort(self,x):
-    j = 1
-    while j < len(x):
-      m = x[j]
-      for i in range(j-1,-1,-1): 
-        if x[i] > m:
-          x[i+1] = x[i]
-          if i == 0:
-            x[0] = m
-        else:
-          x[i+1] = m
+  def sort(self,array):
+    j = 1                           #1
+    while j < len(array):           #2
+      m = array[j]                  #3
+      for i in range(j-1,-1,-1):    #4
+        if array[i] > m:            #5
+          array[i+1] = array[i]
+          if i == 0:                #6
+            array[0] = m
+        else:                       #7
+          array[i+1] = m
           break
       j += 1
-    return x
+    return array
+
+#1 代入を行う要素のインデックス
+#2 最も右の整数の代入が終わるまで繰りかえす
+#3 代入を行う整数の値を保存
+#4 代入する整数と比較を行う整数のインデックスをiとし、左にシフトして比較を行っていく
+#5 比較した整数の方が大きければ、その整数のインデックスを1増やす
+#6 代入する値が最も小さければ配列の1番最初に代入する
+#7 比較した整数の方が小さければ、その整数の後ろに代入する
